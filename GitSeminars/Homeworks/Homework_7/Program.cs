1c853ddd0f1cc129dcc3402114e4ecd7f6edb3a4
Vadim <wadim_fo@mail.ru>
2023-03-10 02:24:21 +0300

    необходимо привезти в нормальный вид домашнее задание номер 7 перед отправкой

--- GitSeminars/Homeworks/Homework_7/Program.cs
+++ GitSeminars/Homeworks/Homework_7/Program.cs
@@ -1,154 +1,182 @@
-﻿// //     // Task47
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-// double[,] create2DRandomArray(int rows, int columns, int minValue, int maxValue)
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// double[,] Create2DRandomArray(int colums, int rows)   // ,-  указывает что массив двумерный, colums - столбы, rows - строки
 // {
-//     double[,] new2DArray = new double[rows,columns];
-//     for(int i = 0; i < rows; i++)
+//     double[,] newArray = new double [rows, colums]; // выделение места под массив, размер массива указывается столбцы на строки
+//     for (int a = 0; a < rows; a++)  // перемещение по рядам;
 //     {
-//         for( int j = 0; j < columns; j++)
+//         for (int b = 0; b < colums; b++) // перемещение по строкам
 //         {
-//             new2DArray[i,j] = Math.Round(new Random().NextDouble(), 2) +  new Random().Next(minValue, maxValue+1);
+//             newArray[a,b] = Math.Round(new Random().NextDouble(), 2) +  new Random().Next(1,10);
 //         }
 //     }
-//     return new2DArray;
-// }
-
-// void print2DRandomArray(double[,] array)
+//     return newArray;
+// }   // Метод для создания и заполнения двумерного массива
+    
+// void Show2dArray(double[,] array)
 // {
-//     for(int i = 0; i < array.GetLength(0); i++)
+//     for (int a = 0; a < array.GetLength(0); a++) // 0 указывает на строку
 //     {
-//         for( int j = 0; j < array.GetLength(1); j++)
+//         for (int b = 0; b < array.GetLength(1); b++) // 1 указывает на столбец
 //         {
-//             Console.Write($"{array[i,j]} ");
-//         }
-//         Console.WriteLine("");
-
+//             Console.Write(array[a, b] + " ");   // выводит наш массив в терминал "" указывает на пробел в сообщении
+//         } // функция Write идет прямо по строке, функция WriteLine Начинает идти с новой строки
+//         Console.WriteLine(); // без этой строки массив указывается в одну строку
 //     }
-// }
+//     Console.WriteLine("");    // при необходимости вывода более одного массива, создает пространство между массивами.
+// }   // метод для вывода двумерного массива
 
-// Console.WriteLine("Input please rows quantity of 2way array");
-//  int n = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input please columns quantity of 2way array");
-//  int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input please minimum number of 2way array");
-//  int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input please maximum number of 2way array");
-//  int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input numbers of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input numbers of colums: ");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Input minimal value of array element");
+// // int minValue = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine("Input maximal value of array element");
+// // int maxValue = Convert.ToInt32(Console.ReadLine());
+// // Строки 28-35 запрос у пользователя информации
 
-//  double[,] arrayFilled = create2DRandomArray(n, m, min, max);
-//  print2DRandomArray(arrayFilled);
+// double [,] array = Create2DRandomArray(rows, colums);
+// Show2dArray(array);
 
-//     // Task50
 
-// int[,] create2DRandomArray(int rows, int columns)
-// {
-//     int[,] new2DArray = new int[rows,columns];
-//     for(int i = 0; i < rows; i++)
-//     {
-//         for( int j = 0; j < columns; j++)
-//         {
-//             new2DArray[i,j] =  new Random().Next(1, 100);
-//         }
-//     }
-//     return new2DArray;
-// }
 
-// void print2DRandomArray(int[,] array)
-// {
-//     for(int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for( int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i,j]} ");
-//         }
-//         Console.WriteLine("");
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
 
-//     }
-// }
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
 
-// void checkMember (int[,] array)
-// {
-//     Console.WriteLine("Input please row number of member");
-//     int rowMember = Convert.ToInt32(Console.ReadLine());
-//     Console.WriteLine("Input please column number of member");
-//     int columnMember = Convert.ToInt32(Console.ReadLine());
-//     if(rowMember > array.GetLength(0) || columnMember > array.GetLength(1))
-//     {
-//         Console.WriteLine($"Mebmer {rowMember}:{columnMember} is out of array");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"Mebmer with adress {rowMember}:{columnMember} started from 0 is {array[rowMember, columnMember]} ");
-//    }
-// }
 
-// Console.WriteLine("Input please rows quantity of 2way array");
-//  int n = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input please columns quantity of 2way array");
-//  int m = Convert.ToInt32(Console.ReadLine());
 
-// int[,] arrayFilled = create2DRandomArray(n, m);
-// print2DRandomArray(arrayFilled);
-// checkMember(arrayFilled);
-
-    // Task52
-
-int[,] create2DRandomArray(int rows, int columns)
+int [,] Create2DRandomArray (int rows, int colums)
 {
-    int[,] new2DArray = new int[rows,columns];
-    for(int i = 0; i < rows; i++)
+    int[,] newArray = new int[rows, colums];
+    for (int a = 0; a < rows; a++)
     {
-        for( int j = 0; j < columns; j++)
+        for (int b = 0; b < colums; b++)
         {
-            new2DArray[i,j] =  new Random().Next(1, 100);
+            newArray[a,b] = new Random().Next(1, 10);
         }
     }
-    return new2DArray;
+    return newArray;
 }
 
-void print2DRandomArray(int[,] array)
+void Show2dArray(int[,] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int a = 0; a < array.GetLength(0); a++)
     {
-        for( int j = 0; j < array.GetLength(1); j++)
+        for (int b = 0; b < array.GetLength(1); b++)
         {
-            Console.Write($"{array[i,j]} ");
+            Console.Write(array[a,b]+ " ");
         }
-        Console.WriteLine("");
-
+        Console.WriteLine(" ");
     }
 }
 
-double[] AverColumnInArray (int[,] array)
+void CheckPosition(int[,] array)
 {
-    double[] averColumnInArray = new double[array.GetLength(1)];
-    for(int i = 0; i <array.GetLength(1); i++)
+    Console.WriteLine("Введите номер ряда");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите номер столба");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    if (rows>array.GetLength(0) || columns > array.GetLength(1))
     {
-        double sum = 0;
-        for(int j = 0; j < array.GetLength(0); j++)
-        {
-            sum += array[j,i];
-        }
-        averColumnInArray[i] = sum / array.GetLength(0);
+        Console.WriteLine($"Введеные значения {rows} и {columns} располагаются за пределами массива");
     }
-    return averColumnInArray;
+    else
+    {
+        Console.WriteLine($"Введеные значения {rows} и {columns} имеет значение {array[rows,columns]}");
+    }
 }
 
-void printArray(double[] array)
-{
-    Console.Write($"Average of all members in columns are : ");
-    for( int i = 0; i < array.Length-1; i++)
-    {
-        Console.Write($"{array[i]}; ");
-    }
-    Console.Write($"{array[array.Length-1]}.");
-}
 
-Console.WriteLine("Input please rows quantity of 2way array");
- int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input please columns quantity of 2way array");
- int m = Convert.ToInt32(Console.ReadLine());
 
-int[,] arrayFilled = create2DRandomArray(n, m);
-print2DRandomArray(arrayFilled);
-printArray(AverColumnInArray(arrayFilled));
+Console.WriteLine("Input numbers of rows: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers of colums: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = Create2DRandomArray(row, column);
+Show2dArray(array);
+CheckPosition(array);
+
+
+
+
+
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int [,] Create2DRandomArray (int rows, int colums)
+// {
+//     int[,] newArray = new int[rows, colums];
+//     for (int a = 0; a < rows; a++)
+//     {
+//         for (int b = 0; b < colums; b++)
+//         {
+//             newArray[a,b] = new Random().Next(1, 10);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2dArray(int[,] array)
+// {
+//     for (int a = 0; a < array.GetLength(0); a++)
+//     {
+//         for (int b = 0; b < array.GetLength(1); b++)
+//         {
+//             Console.Write(array[a,b]+ " ");
+//         }
+//         Console.WriteLine(" ");
+//     }
+// }
+
+// double[] Average (int[,] array)
+// {
+//     double[] average = new double[array.GetLength(1)];
+//     for(int a = 0; a<array.GetLength(1); a++)
+//     { 
+//         double sum = 0;
+//         for (int b = 0; b < array.GetLength(0); b++)
+//         {
+//              sum += array[a , b];
+//         }
+//         average[a] = sum / array.GetLength(0);
+//     }
+//     return average;
+// }
+
+// void printArray(double[] array)
+// {
+//     Console.Write($"Average value of columns : ");
+//     for( int i = 0; i < array.Length-1; i++)
+//     {
+//         Console.Write($"{array[i]}; ");
+//     }
+//     Console.Write($"{array[array.Length-1]}.");
+// }
+
+// Console.WriteLine("Input numbers of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input numbers of colums: ");
+// int colums = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = Create2DRandomArray(rows, colums);
+// Show2dArray(myArray);
+// printArray(Average(myArray));
